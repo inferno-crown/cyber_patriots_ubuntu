f427c91e5b7f02c497ee2d92af0089e23394e793
XZIceCube <85417492+XZIcecube@users.noreply.github.com>
2021-11-07 17:05:24 -0700

    Update all_scripts.py

--- all_scripts.py
+++ all_scripts.py
@@ -1,1 +1,26 @@
+#delete media files
 import os
+media = ["'*.mp3'","'*.mp4'","'*.wav'"]
+
+media = ["'*.mp3'","'*.mp4'","'*.wav'"]
+for i in media:
+        os.system("sudo find $dir -name " +i+ " -delete")
+
+#install and remove packages
+purge = ["netcat","nmap","zemap","john","johntheripper","hydra","metasploit"]
+install = ["rkhunter", "ssh", "ufw", "firefox", "iptables", "Gufw", "lightdm", "bum"]
+for i in install:
+        os.system("sudo apt-get install --assume-yes "+i)
+    
+#install lippam cracklib
+os.system("sudo apt-get update -y")
+os.system("sudo apt-get install -y libpam-cracklib")
+os.system("sudo sudo ufw enable")
+for i in purge:
+        os.system("apt-get purge --assume-yes "+i)
+
+#update packages
+os.system("sudo apt-get update && sudo apt-get upgrade")
+
+#show what is running on the computer
+os.system("ps aux > running.txt")
